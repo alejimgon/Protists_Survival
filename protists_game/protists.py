91811de4618534cbfd9d414e47ec28131bd28c92
Alejandro Jiménez-González <alejimgon@gmail.com>
2025-06-10 10:43:57 +0200

    Adjusting the protist's speed

--- protists_game/protists.py
+++ protists_game/protists.py
@@ -6,10 +6,15 @@ class Protist:
     def __init__(self, ps_game, image_path): # ps_game is an instance of the ProtistSurvival class.
         """Initialize the protist and set its starting position."""
         self.screen = ps_game.screen
+        self.settings = ps_game.settings
         self.screen_rect = ps_game.screen.get_rect()
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
         self.rect.midleft = self.screen_rect.midleft # Position the protist at the left middle of the screen.
+
+        # Store the protist's exact position.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         # Movement flag; start with a protist that's not moving.
         self.moving_right = False
         self.moving_left = False
@@ -18,15 +23,22 @@ class Protist:
     
     def update(self):
         """Update the protist's position based on movement flags."""
+        # Update the protist's position based on the movement flags.
+        # Move the protist in the direction of the movement flags, but only if it doesn't go out of bounds.
+        # Check if the protist is moving in any direction and update its position accordingly.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.x += 1
+            self.x += self.settings.protist_speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.x -= 1
+            self.x -= self.settings.protist_speed
         if self.moving_up and self.rect.top > 0:
-            self.rect.y -= 1
+            self.y -= self.settings.protist_speed
         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-            self.rect.y += 1
+            self.y += self.settings.protist_speed
 
+    # Update rect object from self.x and self.y.
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
--- protists_game/settings.py
+++ protists_game/settings.py
@@ -8,3 +8,5 @@ class Settings:
         self.screen_height = 775 # This is the height of the game screen in pixels.
         self.bg_color = (230, 230, 230)  # This is a tuple that represents the RGB color of the background. The values range from 0 to 255, where 0 means no color and 255 means full color.
 
+        # Protist settings
+        self.protist_speed = 1.5 # This is the speed at which the protists move across the screen. The value is in pixels per frame, meaning that each time the game updates, the protists will move by this number of pixels.
