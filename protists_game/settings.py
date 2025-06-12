28091cac292c976c53c310305e46eb63017ac52e
Alejandro Jiménez-González <alejimgon@gmail.com>
2025-06-12 15:11:38 +0200

    Increase speed

--- protists_game/settings.py
+++ protists_game/settings.py
@@ -6,7 +6,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200 # This is the width of the game screen in pixels.
         self.screen_height = 775 # This is the height of the game screen in pixels.
-        self.bg_color = (230, 230, 230)  # This is a tuple that represents the RGB color of the background. The values range from 0 to 255, where 0 means no color and 255 means full color.
+        self.bg_color = (230, 230, 230)  # This is a tuple that represents the RGB color of the background.
 
         # Protist settings
-        self.protist_speed = 1.5 # This is the speed at which the protists move across the screen. The value is in pixels per frame, meaning that each time the game updates, the protists will move by this number of pixels.
+        self.protist_speed = 2 # This is the speed at which the protists move across the screen. The value is in pixels per frame, meaning that each time the game updates, the protists will move by this number of pixels.
