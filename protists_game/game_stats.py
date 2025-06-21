9b6637279f97930fe228c890a23ba73b19ccd0ef
Alejandro Jiménez-González <alejimgon@gmail.com>
2025-06-21 20:01:35 +0200

    Create game_stats.py

---
+++ protists_game/game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+    """Class to manage game statistics for Protists Game."""
+
+    def __init__(self, ps_game, protist):
+        """Initialize statistics."""
+        self.settings = ps_game.settings
+        self.protist = protist
+        self.danger_defence = protist.danger_defence_max
+        self.reset_stats()
+    
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.lives_left = self.settings.protist_lives
+        self.score = 0
+        self.danger_defence = self.protist.danger_defence_max
